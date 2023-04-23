5495d082373055bb66a986c058ac11cb5befc63d
AlexandruBirta <alexbirta2011@gmail.com>
2023-04-23 18:57:03 +0300

    exceptions

--- src/main/java/ro/unibuc/fmi/airlliantmodel/exception/ExceptionStatus.java
+++ src/main/java/ro/unibuc/fmi/airlliantmodel/exception/ExceptionStatus.java
@@ -11,7 +11,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionStatus {
 
-    AIRLLIANT_INTERNAL_ERROR("Application encountered an unexpected error.", ErrorCode.AIRLLIANT_INTERNAL_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
+    AIRLLIANT_INTERNAL_ERROR("Application encountered an unexpected error.", ErrorCode.AIRLLIANT_INTERNAL_ERROR, HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_NOT_FOUND("User with id '%s' not found!", ErrorCode.USER_NOT_FOUND, HttpStatus.NOT_FOUND),
+    USER_ALREADY_EXISTS("User with id '%s' already exists!", ErrorCode.USER_ALREADY_EXISTS, HttpStatus.BAD_REQUEST);
+
 
     private final String value;
     private final ErrorCode errorCode;
@@ -41,17 +44,22 @@ public enum ExceptionStatus {
 
     @Getter
     @Schema(enumAsRef = true, description = "Error codes description: \n" +
-            "* `AIE_022` - AIRLLIANT_INTERNAL_ERROR\n" +
-            "* `ANF_023` - AIRLLIANT_NOT_FOUND\n" +
-            "* `ABR_024` - AIRLLIANT_BAD_REQUEST\n" +
-            "* `AFB_041` - AIRLLIANT_FORBIDDEN\n" +
+            "* `AIE_001` - AIRLLIANT_INTERNAL_ERROR\n" +
+            "* `ANF_002` - AIRLLIANT_NOT_FOUND\n" +
+            "* `ABR_003` - AIRLLIANT_BAD_REQUEST\n" +
+            "* `AFB_004` - AIRLLIANT_FORBIDDEN\n" +
+            "* `UNF_005` - USER_NOT_FOUND\n" +
+            "* `UAE_006` - USER_ALREADY_EXISTS\n" +
             "")
     public enum ErrorCode {
 
-        AIRLLIANT_INTERNAL_ERROR("AIE_022"),
-        AIRLLIANT_NOT_FOUND("ANF_023"),
-        AIRLLIANT_BAD_REQUEST("ABR_024"),
-        AIRLLIANT_FORBIDDEN("AFB_041");
+        AIRLLIANT_INTERNAL_ERROR("AIE_001"),
+        AIRLLIANT_NOT_FOUND("ANF_002"),
+        AIRLLIANT_BAD_REQUEST("ABR_003"),
+        AIRLLIANT_FORBIDDEN("AFB_004"),
+        USER_NOT_FOUND("UNF_005"),
+        USER_ALREADY_EXISTS("UAE_006");
+
         private final String value;
 
         ErrorCode(String value) {
