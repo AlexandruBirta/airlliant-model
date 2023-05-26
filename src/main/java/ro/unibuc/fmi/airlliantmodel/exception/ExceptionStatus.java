cb7b5d9ccba8198de536838af1f18fb261ea30be
AlexandruBirta <alexbirta2011@gmail.com>
2023-05-26 19:31:50 +0300

    exception

--- src/main/java/ro/unibuc/fmi/airlliantmodel/exception/ExceptionStatus.java
+++ src/main/java/ro/unibuc/fmi/airlliantmodel/exception/ExceptionStatus.java
@@ -17,11 +17,12 @@ public enum ExceptionStatus {
     FLIGHT_NOT_FOUND("Flight with id '%s' not found!", ErrorCode.FLIGHT_NOT_FOUND, HttpStatus.NOT_FOUND),
     FLIGHT_ALREADY_EXISTS("Flight with number '%s' already exists!", ErrorCode.FLIGHT_ALREADY_EXISTS, HttpStatus.BAD_REQUEST),
     TICKET_NOT_FOUND("Ticket with id '%s' not found!", ErrorCode.TICKET_NOT_FOUND, HttpStatus.NOT_FOUND),
-    TICKET_ALREADY_EXISTS("Ticket with user id '%s' and flight id '%s' already exists for the selected seat row '%s' and seat number '%s'!", ErrorCode.TICKET_ALREADY_EXISTS, HttpStatus.BAD_REQUEST),
+    TICKET_ALREADY_EXISTS("Ticket for user with id '%s' and flight id '%s' already exists for the selected seat row '%s' and seat number '%s'!", ErrorCode.TICKET_ALREADY_EXISTS, HttpStatus.BAD_REQUEST),
     ADD_TICKET_TRIGGER_ERROR("Error during trigger creation for ticket with id '%s'.", ErrorCode.ADD_TICKET_TRIGGER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR),
     DELETE_TICKET_TRIGGER_ERROR("Error during trigger deletion for ticket with id '%s'.", ErrorCode.DELETE_TICKET_TRIGGER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR),
     FLIGHT_ALREADY_DEPARTED("Flight with id '%s' already departed on '%s'!", ErrorCode.FLIGHT_ALREADY_DEPARTED, HttpStatus.BAD_REQUEST),
-    USER_NOT_FOUND_BY_EMAIL("User with email '%s' not found!", ErrorCode.USER_NOT_FOUND_BY_EMAIL, HttpStatus.NOT_FOUND);
+    USER_NOT_FOUND_BY_EMAIL("User with email '%s' not found!", ErrorCode.USER_NOT_FOUND_BY_EMAIL, HttpStatus.NOT_FOUND),
+    TICKET_ALREADY_EXISTS_FOR_EMAIL("Ticket for user with email '%s' and flight id '%s' already exists for the selected seat row '%s' and seat number '%s'!", ErrorCode.TICKET_ALREADY_EXISTS_FOR_EMAIL, HttpStatus.BAD_REQUEST);
 
 
     private final String value;
@@ -65,7 +66,8 @@ public enum ExceptionStatus {
             "* `ATE_011` - ADD_TICKET_TRIGGER_ERROR\n" +
             "* `DTE_011` - DELETE_TICKET_TRIGGER_ERROR\n" +
             "* `FAD_012` - FLIGHT_ALREADY_DEPARTED\n" +
-            "* `UNF_013` - USER_NOT_FOUND_BY_EMAIL\n")
+            "* `UNF_013` - USER_NOT_FOUND_BY_EMAIL\n" +
+            "* `TAE_014` - TICKET_ALREADY_EXISTS_FOR_EMAIL\n")
     public enum ErrorCode {
 
         AIRLLIANT_INTERNAL_ERROR("AIE_001"),
@@ -81,7 +83,8 @@ public enum ExceptionStatus {
         ADD_TICKET_TRIGGER_ERROR("ATE_011"),
         DELETE_TICKET_TRIGGER_ERROR("DTE_011"),
         FLIGHT_ALREADY_DEPARTED("FAD_012"),
-        USER_NOT_FOUND_BY_EMAIL("UNF_013");
+        USER_NOT_FOUND_BY_EMAIL("UNF_013"),
+        TICKET_ALREADY_EXISTS_FOR_EMAIL("TAE_014");
 
 
         private final String value;
